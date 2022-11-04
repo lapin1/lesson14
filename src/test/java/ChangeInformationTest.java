3f8c9fcbedaea6b6e949c5f2663a6b795d188a20
Yauheni <91690000+lapin1@users.noreply.github.com>
2022-11-04 21:36:22 +0100

    Update ChangeInformationTest.java

--- src/test/java/ChangeInformationTest.java
+++ src/test/java/ChangeInformationTest.java
@@ -1,5 +1,7 @@
 import com.andersenlab.ChangeInformationPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,18 +11,21 @@ import org.openqa.selenium.WebDriver;
 public class ChangeInformationTest {
     WebDriver driver;
 
+    @Step("Start")
     @BeforeEach
     void setup() {
         driver = WebDriverManager.chromedriver().create();
         driver.get("http://automationpractice.com/index.php");
     }
 
+    @Step("Stop")
     @AfterEach
     void teardown() {
         driver.quit();
     }
 
     @Test
+    @Description("Change user information")
     public void changeInformation() {
         String text = "Your personal information has been successfully updated.";
         new ChangeInformationPage(driver)
