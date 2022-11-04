01211104f1d2433efafc09e309e33100929cfa67
Yauheni <91690000+lapin1@users.noreply.github.com>
2022-11-04 21:34:06 +0100

    Update ChangeAddressTest.java

--- src/test/java/ChangeAddressTest.java
+++ src/test/java/ChangeAddressTest.java
@@ -1,5 +1,7 @@
 import com.andersenlab.ChangeAddressPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,18 +10,21 @@ import org.openqa.selenium.WebDriver;
 public class ChangeAddressTest {
     WebDriver driver;
 
+    @Step("Start")
     @BeforeEach
     void setup() {
         driver = WebDriverManager.chromedriver().create();
         driver.get("http://automationpractice.com/index.php");
     }
 
+    @Step("Stop")
     @AfterEach
     void teardown() {
         driver.quit();
     }
 
     @Test
+    @Description("Change address test")
     public void changeAddress() {
         new ChangeAddressPage(driver)
                 .clickSignInButton()
