b20f02bc3febcf43fc1aaba26c9a9726229e0c27
Yauheni <91690000+lapin1@users.noreply.github.com>
2022-11-04 21:38:40 +0100

    Update ProductPurchaseTest.java

--- src/test/java/ProductPurchaseTest.java
+++ src/test/java/ProductPurchaseTest.java
@@ -1,5 +1,7 @@
 import com.andersenlab.ProductPurchasePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,18 +10,21 @@ import org.openqa.selenium.WebDriver;
 public class ProductPurchaseTest {
     WebDriver driver;
 
+    @Step("Start")
     @BeforeEach
     void setup() {
         driver = WebDriverManager.chromedriver().create();
         driver.get("http://automationpractice.com/index.php");
     }
 
+    @Step("Stop")
     @AfterEach
     void teardown() {
         driver.quit();
     }
 
     @Test
+    @Description("Product purchase")
     public void productPurchase() {
         new ProductPurchasePage(driver)
                 .selectProduct()
